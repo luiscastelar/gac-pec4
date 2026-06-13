833d30c395ebcab1829342f2240a8d7fafbc243f
Luis Ferreira (casa) <luisferreira@iescastelar.com>
2026-06-13 07:49:44 +0200

    add app test amplia

--- test_app.py
+++ test_app.py
@@ -4,8 +4,9 @@ import requests, json, time
 
 URL = 'http://localhost:5000/API/v1'
 tablas = ["alumnos", "calificaciones"]
+tablas = ["alumnos"]
 metodos = {
-    "get": [None, 0, 1, 3, 50, -1],
+    "get": [None, 0, 1, 3, 50, -1, "1"],
     "post": [
         {"nombre": "Pedro", "apellidos": "Pérez", "edad": 24},
         {"nombre": "Juan", "apellidos": "García"},
@@ -21,45 +22,67 @@ metodos = {
         {"id": -2},
         {}
     ],
+    "raro": [1, "b"],
 }
 
 
 def wordker(coleccion, metodo, carga):
-    print(f'{coleccion}->{metodo}: {carga}')
-    match metodo:
-        case 'get':
-            if carga is None:
-                peticion = f'{URL}/{coleccion}'
-            else:
-                peticion = f'{URL}/{coleccion}/{carga}'
-            print(peticion)
-            response = requests.get(peticion)
-            if response.headers['content-type'] == 'application/json; charset=utf8':
-                print(response.json())
-            else:
-                print(f'[{response.status_code}]: {response.content}')
-            
-        case 'post':
-            print(f'GET {coleccion}')
-        case 'put':
-            print(f'PUT {coleccion}')
-        case 'delete':
-            print(f'DELETE {coleccion}')
-        case _:
-            print('Error!!!!')
+    print(f'\n{coleccion}->{metodo}: {carga}')
+    response = None
+    try:
+        match metodo:
+            case 'get':
+                if carga is None:
+                    peticion = f'{URL}/{coleccion}'
+                else:
+                    peticion = f'{URL}/{coleccion}/{carga}'
+                print(peticion)
+                response = requests.get(peticion)
 
+            case 'post':
+                response = requests.post(URL, json={
+                    "coleccion": coleccion,
+                    "carga": carga
+                })
+            case 'put':
+                response = requests.put(URL, json={
+                    "coleccion": coleccion,
+                    "carga": carga
+                })
+            case 'delete':
+                response = requests.delete(URL, json={
+                    "coleccion": coleccion,
+                    "id": carga.get("id", None)
+                })
+            case _:
+                raise NotImplementedError(f'Pendiente de implementar {metodo.upper()}')
 
+        if response.headers['content-type'] == 'application/json; charset=utf8':
+            print(response.json())
+        else:
+            print(f'[{response.status_code}]: {response.content}')
+    except Exception as exc:
+        print(f'Error {exc}')
+
+threads_running = []
 for metodo in metodos:
     for tabla in tablas:
         for carga in metodos[metodo]:
-            Thread(
+            t = Thread(
                 target=wordker,
-                name=f'{tabla}->{metodo}',
+                name=f'{tabla}->{metodo}({carga})',
                 kwargs={
                     "coleccion": tabla,
                     "metodo": metodo,
                     "carga": carga
                 },
                 daemon=True
-            ).start()
-            time.sleep(5)
+            )
+            threads_running.append(t)
+            t.start()
+            time.sleep(1.2)
+
+# Wait for all threads to finish
+for t in threads_running:
+    print(f'Esperando a "{t.name}"')
+    t.join()
